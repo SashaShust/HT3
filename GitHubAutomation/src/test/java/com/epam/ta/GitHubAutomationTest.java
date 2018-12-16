ffa628d438e22d794602436b7cd425f24a60b300
SashaShust <akuzmenko001@gmail.com>
2018-12-16 16:09:56 +0300

    first

---
+++ GitHubAutomation/src/test/java/com/epam/ta/GitHubAutomationTest.java
@@ -0,0 +1,71 @@
+package com.epam.ta;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.epam.ta.steps.Steps;
+import com.epam.ta.utils.Utils;
+
+public class GitHubAutomationTest {
+	private Steps steps;
+	private final String USERNAME = "testautomationuser";
+	private final String PASSWORD = "Time4Death!";
+	private final int REPOSITORY_NAME_POSTFIX_LENGTH = 6;
+
+	private final String EMPTY_USERNAME = "";
+	private final String EMPTY_PASSWORD = "";
+	private final String TEXT_MESSAGE = "Incorrect username or password.";
+
+	@BeforeMethod(description = "Init browser")
+	public void setUp() {
+		steps = new Steps();
+		steps.openBrowser();
+
+	}
+
+	@Test
+	public void oneCanCreateProject() {
+		steps.loginGithub(USERNAME, PASSWORD);
+		String repositoryName = steps.generateRandomRepositoryNameWithCharLength(REPOSITORY_NAME_POSTFIX_LENGTH);
+		steps.createNewRepository(repositoryName, "auto-generated test repo");
+		Assert.assertEquals(steps.getCurrentRepositoryName(), repositoryName);
+	}
+
+	@Test
+	public void oneCanCreateNewEmptyRepository() {
+		steps.loginGithub(USERNAME, PASSWORD);
+		String repositoryName = Utils.getRandomString(REPOSITORY_NAME_POSTFIX_LENGTH);
+		steps.createNewRepository(repositoryName, "auto-generated test repo");
+		assertTrue(steps.currentRepositoryIsEmpty());
+	}
+
+	@Test
+	public void oneCannotLoginGithub() {
+		steps.loginGithub(EMPTY_USERNAME, EMPTY_PASSWORD);
+		assertEquals(steps.getTextMessage(), TEXT_MESSAGE);
+	}
+
+	@Test
+	public void oneCannotCreateUntitledRepository() {
+		steps.loginGithub(USERNAME, PASSWORD);
+		// steps.createNewUntitledRepository();
+		assertFalse(steps.createNewUntitledRepository());
+	}
+
+	@Test(description = "Login to Github")
+	public void oneCanLoginGithub() {
+		steps.loginGithub(USERNAME, PASSWORD);
+		Assert.assertEquals(USERNAME, steps.getLoggedInUserName());
+	}
+
+	@AfterMethod(description = "Stop Browser")
+	public void stopBrowser() {
+		steps.closeBrowser();
+	}
+}
---
+++ GitHubAutomation/src/test/java/com/epam/ta/steps/Steps.java
@@ -0,0 +1,72 @@
+package com.epam.ta.steps;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import com.epam.ta.driver.DriverSingleton;
+import com.epam.ta.pages.CreateNewRepositoryPage;
+import com.epam.ta.pages.LoginPage;
+import com.epam.ta.pages.MainPage;
+import com.epam.ta.utils.Utils;
+
+public class Steps {
+	private WebDriver driver;
+
+	private final Logger logger = LogManager.getRootLogger();
+
+	public void openBrowser() {
+		driver = DriverSingleton.getDriver();
+	}
+
+	public void closeBrowser() {
+		DriverSingleton.closeDriver();
+	}
+
+	public void loginGithub(String username, String password) {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.openPage();
+		loginPage.login(username, password);
+	}
+
+	public String getLoggedInUserName() {
+		LoginPage loginPage = new LoginPage(driver);
+		return loginPage.getLoggedInUserName().trim().toLowerCase();
+	}
+
+	public void createNewRepository(String repositoryName, String repositoryDescription) {
+		MainPage mainPage = new MainPage(driver);
+		mainPage.clickOnCreateNewRepositoryButton();
+		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+		createNewRepositoryPage.createNewRepository(repositoryName, repositoryDescription);
+	}
+
+	public boolean currentRepositoryIsEmpty() {
+		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+		return createNewRepositoryPage.isCurrentRepositoryEmpty();
+	}
+
+	public String getCurrentRepositoryName() {
+		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+		return createNewRepositoryPage.getCurrentRepositoryName();
+	}
+
+	public String generateRandomRepositoryNameWithCharLength(int howManyChars) {
+		String repositoryNamePrefix = "testRepo_";
+		return repositoryNamePrefix.concat(Utils.getRandomString(howManyChars));
+	}
+
+	public String getTextMessage() {
+		LoginPage loginPage = new LoginPage(driver);
+		return loginPage.getTextMessage().trim();
+	}
+
+	public boolean createNewUntitledRepository() {
+		MainPage mainPage = new MainPage(driver);
+		mainPage.clickOnCreateNewRepositoryButton();
+		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+		createNewRepositoryPage.getinputRepositoryName().clear();
+		return createNewRepositoryPage.getButttonCreate().isEnabled();
+	}
+
+}
